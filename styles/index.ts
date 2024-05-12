fde3cf08a18a6f5c1c186142e3b91ddebba79537
vaaghu <vaaghu0@gmail.com>
2024-05-12 11:58:13 +0530

    init

---
+++ styles/index.ts
@@ -0,0 +1,3 @@
+export { default as initialSectionStyle } from "./initialSection.module.scss";
+export { default as indexStyle } from "./index.module.scss";
+export { default as workComponentStyle } from "./works.module.scss";
